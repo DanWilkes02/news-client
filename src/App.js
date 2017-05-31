67e1d7b649d57754b076675f8ab1ecaa5f11abf3
DanWilkes02 <daniel_wilkes@outlook.com>
2017-05-31 21:51:44 +0100

    Working API connection to News API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      headline: '',
+    }
+  }
+
+  componentDidMount(){
+    console.log('didMount');
+    axios.get('https://newsapi.org/v1/articles?source=techcrunch&apiKey=2f0e768750294d9a886f63de3db38fd0')
+    .then((res) => {
+      let headline = res.data.articles[0].title;
+      console.log(headline);
+      this.setState({headline: headline});
+    });
+  }
+
   render() {
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{this.state.headline}</h1>
       </div>
     );
   }
